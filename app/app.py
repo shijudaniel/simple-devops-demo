37885b530c0068caa99d7e0c1c5ed4c1c0bee41c
shiju <shijudaniel@gmail.com>
2026-05-07 10:52:22 -0400

    bad code

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Demo App Running!"
+    user_input = request.args.get("input")
+    eval(user_input)
+    return "Unsafe Code"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
