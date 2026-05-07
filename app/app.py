6d4e7b418779e5c3360ea900f162855f4c9d0469
shiju <shijudaniel@gmail.com>
2026-05-07 11:11:44 -0400

    fixed code

--- app/app.py
+++ app/app.py
@@ -1,12 +1,10 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    user_input = request.args.get("input")
-    eval(user_input)
-    return "Unsafe Code"
+    return "DevOps Demo App Running!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
