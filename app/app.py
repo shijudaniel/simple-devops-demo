11965fc8772f1aab6584fd923c211f9823b9491e
shiju <shijudaniel@gmail.com>
2026-05-09 13:05:31 -0400

    5th added more spaces-more

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return "HELLO .._.. DevOps Demo App Running! AGAIN ...-5-..."
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
